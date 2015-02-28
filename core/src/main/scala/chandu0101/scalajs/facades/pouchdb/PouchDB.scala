5e7e7d9a4740ac207e27d34da42d611b4015409d
Otto Chrons <otto.chrons@microtask.com>
2015-02-28 23:06:02 +0200

    Simplified calling of Pouch methods by moving common code into a wrapper function.
    Change-Id: Id650f3c717da9e152d8e1862ea671ce373b3600b

--- core/src/main/scala/chandu0101/scalajs/facades/pouchdb/PouchDB.scala
+++ core/src/main/scala/chandu0101/scalajs/facades/pouchdb/PouchDB.scala
@@ -14,77 +14,46 @@ class PouchDB(pouchDBJS : PouchDBJS) {
 
   val name : String = pouchDBJS._db_name
 
-  def get(docId: String, options: js.UndefOr[GetOptions] = GetOptions()) : Future[js.Dynamic] = {
-    val promise = Promise[js.Dynamic]
-    pouchDBJS.getWithCallback(docId,options,callback = (err : js.UndefOr[js.Dynamic], response :js.UndefOr[js.Dynamic]) => {
-      if(js.isUndefined(err) || err == null) promise.success(response.get)
+  def wrapPouchCall(f: (PouchDBJS.CALLBACK) => Unit) = {
+    val promise = Promise[js.Dynamic]()
+    f((err: js.UndefOr[js.Dynamic], response: js.UndefOr[js.Dynamic]) => {
+      if (js.isUndefined(err) || err == null) promise.success(response.get)
       else promise.failure(PouchDBException(err.get))
     })
     promise.future
+  }
+
+  def get(docId: String, options: js.UndefOr[GetOptions] = GetOptions()) : Future[js.Dynamic] = {
+    wrapPouchCall(cb => pouchDBJS.getWithCallback(docId,options,cb))
   }
 
   def destroy(options: js.UndefOr[js.Dynamic] = js.undefined): Future[js.Dynamic] = {
-    val promise = Promise[js.Dynamic]
-    pouchDBJS.destroy((err : js.UndefOr[js.Dynamic], response :js.UndefOr[js.Dynamic]) => {
-      if(js.isUndefined(err) || err == null) promise.success(response.get)
-      else promise.failure(PouchDBException(err.get))
-    })
-    promise.future
+    wrapPouchCall(cb => pouchDBJS.destroy(cb))
   }
 
   def put(doc: js.Dynamic, docId: js.UndefOr[String] = js.undefined, docRev: js.UndefOr[String] = js.undefined, options: js.UndefOr[js.Object] = js.undefined): Future[js.Dynamic] = {
-    val promise = Promise[js.Dynamic]
-    pouchDBJS.put(doc,docId,docRev,options,(err : js.UndefOr[js.Dynamic], response :js.UndefOr[js.Dynamic]) => {
-      if(js.isUndefined(err) || err == null) promise.success(response.get)
-      else promise.failure(PouchDBException(err.get))
-    })
-    promise.future
+    wrapPouchCall(cb => pouchDBJS.put(doc,docId,docRev,options,cb))
   }
 
   def post(doc: js.Dynamic, options: js.UndefOr[js.Dynamic] = js.undefined): Future[js.Dynamic] = {
-    val promise = Promise[js.Dynamic]
-    pouchDBJS.post(doc,options,(err : js.UndefOr[js.Dynamic], response :js.UndefOr[js.Dynamic]) => {
-      if(js.isUndefined(err) || err == null) promise.success(response.get)
-      else promise.failure(PouchDBException(err.get))
-    })
-    promise.future
+    wrapPouchCall(cb => pouchDBJS.post(doc,options,cb))
   }
 
   def remove(doc: js.Object, options: UndefOr[js.Dynamic] = js.undefined): Future[js.Dynamic] = {
-    val promise = Promise[js.Dynamic]
-    pouchDBJS.remove(doc,options,(err : js.UndefOr[js.Dynamic], response :js.UndefOr[js.Dynamic]) => {
-      if(js.isUndefined(err) || err == null) promise.success(response.get)
-      else promise.failure(PouchDBException(err.get))
-    })
-    promise.future
+    wrapPouchCall(cb => pouchDBJS.remove(doc,options,cb))
   }
 
   def removeWithId(docId: String, docRev : String, options: UndefOr[js.Dynamic] = js.undefined): Unit = {
-    val promise = Promise[js.Dynamic]
-    pouchDBJS.removeWithId(docId,docRev,options,(err : js.UndefOr[js.Dynamic], response :js.UndefOr[js.Dynamic]) => {
-      if(js.isUndefined(err) || err == null) promise.success(response.get)
-      else promise.failure(PouchDBException(err.get))
-    })
-    promise.future
+    wrapPouchCall(cb => pouchDBJS.removeWithId(docId,docRev,options,cb))
   }
 
 
   def bulkDocs(docs: js.Any, options: UndefOr[js.Dynamic] = js.undefined): Future[js.Dynamic] = {
-    val promise = Promise[js.Dynamic]
-    pouchDBJS.bulkDocs(docs,options,(err : js.UndefOr[js.Dynamic], response :js.UndefOr[js.Dynamic]) => {
-      if(js.isUndefined(err) || err == null) promise.success(response.get)
-      else promise.failure(PouchDBException(err.get))
-    })
-    promise.future
+    wrapPouchCall(cb => pouchDBJS.bulkDocs(docs,options,cb))
   }
 
   def allDocs(options: AllDocsOptions = AllDocsOptions()): Future[js.Dynamic] = {
-    val promise = Promise[js.Dynamic]
-    pouchDBJS.allDocs(options,(err : js.UndefOr[js.Dynamic], response :js.UndefOr[js.Dynamic]) => {
-      if(js.isUndefined(err) || err == null) promise.success(response.get)
-      else promise.failure(PouchDBException(err.get))
-    })
-    promise.future
+    wrapPouchCall(cb => pouchDBJS.allDocs(options,cb))
   }
 
   def changes(options : ChangesOptions = ChangesOptions()) : ChangesEventEmitter = {
@@ -94,75 +63,35 @@ class PouchDB(pouchDBJS : PouchDBJS) {
   val replicate: Replicate = pouchDBJS.replicate
 
   def putAttachment(docId: String, attachmentId: String, rev: String, attachment: dom.Blob, `type`: String): Future[js.Dynamic] = {
-    val promise = Promise[js.Dynamic]
-    pouchDBJS.putAttachment(docId,attachmentId,rev,attachment,`type`,(err : js.UndefOr[js.Dynamic], response :js.UndefOr[js.Dynamic]) => {
-      if(js.isUndefined(err) || err == null) promise.success(response.get)
-      else promise.failure(PouchDBException(err.get))
-    })
-    promise.future
+    wrapPouchCall(cb => pouchDBJS.putAttachment(docId,attachmentId,rev,attachment,`type`,cb))
   }
 
   def getAttachment(docId: String, attachmentId: String, options: UndefOr[js.Dynamic] = js.undefined): Future[js.Dynamic] = {
-    val promise = Promise[js.Dynamic]
-    pouchDBJS.getAttachment(docId,attachmentId,options,(err : js.UndefOr[js.Dynamic], response :js.UndefOr[js.Dynamic]) => {
-      if(js.isUndefined(err) || err == null) promise.success(response.get)
-      else promise.failure(PouchDBException(err.get))
-    })
-    promise.future
+    wrapPouchCall(cb => pouchDBJS.getAttachment(docId,attachmentId,options,cb))
   }
 
   def removeAttachment(docId: String, attachmentId: String, rev: String): Future[js.Dynamic] = {
-    val promise = Promise[js.Dynamic]
-    pouchDBJS.removeAttachment(docId,attachmentId,rev,(err : js.UndefOr[js.Dynamic], response :js.UndefOr[js.Dynamic]) => {
-      if(js.isUndefined(err) || err == null) promise.success(response.get)
-      else promise.failure(PouchDBException(err.get))
-    })
-    promise.future
+    wrapPouchCall(cb => pouchDBJS.removeAttachment(docId,attachmentId,rev,cb))
   }
 
   def query(fun: js.Function, options: QueryOptions = QueryOptions()): Future[js.Dynamic] = {
-    val promise = Promise[js.Dynamic]
-    pouchDBJS.query(fun,options,(err : js.UndefOr[js.Dynamic], response :js.UndefOr[js.Dynamic]) => {
-      if(js.isUndefined(err) || err == null) promise.success(response.get)
-      else promise.failure(PouchDBException(err.get))
-    })
-    promise.future
+    wrapPouchCall(cb => pouchDBJS.query(fun,options,cb))
   }
 
   def viewCleanup(options: UndefOr[js.Dynamic] = js.undefined): Future[js.Dynamic] = {
-    val promise = Promise[js.Dynamic]
-    pouchDBJS.viewCleanup(options,(err : js.UndefOr[js.Dynamic], response :js.UndefOr[js.Dynamic]) => {
-      if(js.isUndefined(err) || err == null) promise.success(response.get)
-      else promise.failure(PouchDBException(err.get))
-    })
-    promise.future
+    wrapPouchCall(cb => pouchDBJS.viewCleanup(options,cb))
   }
 
   def info: Future[js.Dynamic] = {
-    val promise = Promise[js.Dynamic]
-    pouchDBJS.info((err : js.UndefOr[js.Dynamic], response :js.UndefOr[js.Dynamic]) => {
-      if(js.isUndefined(err) || err == null) promise.success(response.get)
-      else promise.failure(PouchDBException(err.get))
-    })
-    promise.future
+    wrapPouchCall(cb => pouchDBJS.info(cb))
   }
 
   def compact(options: UndefOr[js.Dynamic] = js.undefined) :Future[js.Dynamic] = {
-    val promise = Promise[js.Dynamic]
-    pouchDBJS.compact(options,(err : js.UndefOr[js.Dynamic], response :js.UndefOr[js.Dynamic]) => {
-      if(js.isUndefined(err) || err == null) promise.success(response.get)
-      else promise.failure(PouchDBException(err.get))
-    })
-    promise.future
+    wrapPouchCall(cb => pouchDBJS.compact(options,cb))
   }
 
   def revsDiff(diff: js.Array[String]):Future[js.Dynamic] = {
-    val promise = Promise[js.Dynamic]
-    pouchDBJS.revsDiff(diff,(err : js.UndefOr[js.Dynamic], response :js.UndefOr[js.Dynamic]) => {
-      if(js.isUndefined(err) || err == null) promise.success(response.get)
-      else promise.failure(PouchDBException(err.get))
-    })
-    promise.future
+    wrapPouchCall(cb => pouchDBJS.revsDiff(diff,cb))
   }
 
   def sync(remoteDB: String, options: ReplicateOptions = ReplicateOptions()): ChangesEventEmitter = pouchDBJS.sync(remoteDB,options)
