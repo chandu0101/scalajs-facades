d0500f6c815cad4d5cb3472c4488d896ea7aaa62
Rikard Mustajärvi <rikard.mustajarvi@gmail.com>
2015-05-30 12:04:52 +0200

    Make constructor parameters of LLatLng and LPoint public

--- core/src/main/scala/chandu0101/scalajs/facades/leaflet/LeaftLet.scala
+++ core/src/main/scala/chandu0101/scalajs/facades/leaflet/LeaftLet.scala
@@ -14,7 +14,7 @@ import scala.scalajs.js.{UndefOr, undefined}
 
 
 @JSName("L.LatLng")
-class LLatLng(lat: Double, lng: Double, alt: UndefOr[Double]) extends js.Object {
+class LLatLng(val lat: Double, val lng: Double, alt: UndefOr[Double]) extends js.Object {
   def this(lat: Double, lng: Double) = this(lat, lng, undefined)
 
   def distanceTo(other: LLatLng): Double = js.native
@@ -31,7 +31,7 @@ object LLatLng {
 
 
 @JSName("L.Point")
-class LPoint(x: Double, y: Double, roundB: UndefOr[Boolean]) extends js.Object {
+class LPoint(val x: Double, val y: Double, val roundB: UndefOr[Boolean]) extends js.Object {
   def this(x: Double, y: Double) = this(x, y, undefined)
 
   def add(other: LPoint): LPoint = js.native
